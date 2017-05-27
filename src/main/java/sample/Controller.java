934019bdd3ce5d2c03040fb7a808cd96e6e4daa0
corto <gnot99@gmail.com>
2017-05-28 02:42:39 +0800

    Add skeleton for all components in Capture tab.

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,38 +2,136 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable{
 
-    @FXML private CheckBox startup_launch;
-    @FXML private CheckBox startup_rec;
-    @FXML private Button button_dir;
-    @FXML private TextField text_ssDir;
-    @FXML private TextField text_vidDir;
-    @FXML private Button button_save;
-    @FXML private Button button_reset;
+    @FXML
+    private FlowPane root;
 
-    private File ssDir;
-    private File vidDir;
+    @FXML
+    private ToggleButton bt_start;
+
+    @FXML
+    private CheckBox cb_ss;
+
+    @FXML
+    private CheckBox cb_wc;
+
+    @FXML
+    private Button bt_force;
+
+    @FXML
+    private Button bt_ssConfig;
+
+    @FXML
+    private Button bt_wcConfig;
+
+    @FXML
+    private Button bt_regHotkey;
+
+    @FXML
+    private TextField tf_hotkey;
+
+    @FXML
+    private TextField tf_interval;
+
+    @FXML
+    private DatePicker dp_start;
+
+    @FXML
+    private DatePicker dp_end;
+
+    @FXML
+    private CheckBox cb_schedule;
+
+    @FXML
+    private TextField tf_src;
+
+    @FXML
+    private TextField tf_out;
+
+    @FXML
+    private Button bt_src;
+
+    @FXML
+    private Button bt_out;
+
+    @FXML
+    private TextField tf_width;
+
+    @FXML
+    private TextField tf_height;
+
+    @FXML
+    private CheckBox cb_resize;
+
+    @FXML
+    private CheckBox cb_rename;
+
+    @FXML
+    private Button bt_convert;
+
+    @FXML
+    private CheckBox cb_audio;
+
+    @FXML
+    private ComboBox<?> combo_codec;
+
+    @FXML
+    private Button bt_calculate;
+
+    @FXML
+    private Button bt_render;
+
+    @FXML
+    private TextField tf_renderOut;
+
+    @FXML
+    private TextField tf_renderSrc;
+
+    @FXML
+    private TextField tf_audioFile;
+
+    @FXML
+    private TextField tf_renderFrames;
+
 
     public void initialize(URL url, ResourceBundle rb){
 
-        button_save.setOnMouseClicked(x -> saveSettings());
-        button_reset.setOnMouseClicked(x -> {
-            ssDir = null;
-            vidDir = null;
-            startup_launch.setSelected(false);
-            startup_rec.setSelected(false);
-            text_ssDir.setText("");
-            saveSettings();
+        // Capture Tab
+        bt_start.setOnMouseClicked(x -> Capture.toggle());
+        cb_ss.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            cb_ss.isSelected() ? Settings.enableSS(true) : Settings.enableSS(false);
         });
+        cb_wc.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            cb_wc.isSelected() ? Settings.enableWC(true) : Settings.enableWC(false);
+        });
+        bt_force.setOnMouseClicked(x -> Capture.force());
+        bt_ssConfig.setOnMouseClicked(SSConfig.showDialog());
+        bt_wcConfig.setOnMouseClicked(WCConfig.showDialog());
+        bt_regHotkey.setOnMouseClicked(x -> {
+            String hotkey = KeyEvent.KEY_PRESSED.getName();
+            Settings.setCaptureHotkey(hotkey);
+            tf_hotkey.setText(hotkey);
+        });
+
+        tf_interval.textProperty().addListener((observable, oldValue, newValue) -> {
+            Scanner s = new Scanner(newValue);
+            if (s.hasNextInt()){
+                int i = s.nextInt();
+                if (i > 0 && i < Integer.MAX_VALUE)
+                    Settings.setCaptureInterval(i);
+            }
+        });
+
+        // Schedule Tab
     }
 
     void saveSettings(){
